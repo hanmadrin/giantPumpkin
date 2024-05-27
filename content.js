85ff8a24bd34b13861f6dfd3c9e914d22a9520b7
hanmadrin <hanmadrin@gmail.com>
2024-05-27 08:07:33 +0600

    save

--- content.js
+++ content.js
@@ -1501,7 +1501,7 @@ function mapConfigurationMilesMainButtonSelector(){
 function mapConfigurationMilesButtonSelector(){
     // a button on click will choose a mile from other options
     // x1i10hfl xjbqb8w x1ejq31n xd10rxx x1sy0etr x17r0tee x972fbf xcfux6l x1qhh985 xm0m39n xe8uvvx x1hl2dhg xggy1nq x1o1ewxj x3x9cwd x1e5q0jg x13rtm0m x87ps6o x1lku1pv x1a2a7pz x6s0dn4 xjyslct x9f619 x1ypdohk x78zum5 x1q0g3np x2lah0s xnqzcj9 x1gh759c x1i6fsjq xfvfia3 x1344otq x1de53dj x10wwi4t x1x7e7qh x1n2onr6 x16tdsg8 x1ja2u2z
-    return document.querySelectorAll(`.${' x1i10hfl xjbqb8w x1ejq31n xd10rxx x1sy0etr x17r0tee x972fbf xcfux6l x1qhh985 xm0m39n xe8uvvx x1hl2dhg xggy1nq x1o1ewxj x3x9cwd x1e5q0jg x13rtm0m x87ps6o x1lku1pv x1a2a7pz x6s0dn4 xjyslct x9f619 x1ypdohk x78zum5 x1q0g3np x2lah0s xnqzcj9 x1gh759c x1i6fsjq xfvfia3 x1344otq x1de53dj x10wwi4t x1x7e7qh x1n2onr6 x16tdsg8 x1ja2u2z'.split(' ').join('.')}`);
+    return document.querySelectorAll(`.${'x1i10hfl xjbqb8w x1ejq31n xd10rxx x1sy0etr x17r0tee x972fbf xcfux6l x1qhh985 xm0m39n xe8uvvx x1hl2dhg xggy1nq x1o1ewxj x3x9cwd x1e5q0jg x13rtm0m x87ps6o x1lku1pv x1a2a7pz x6s0dn4 xjyslct x9f619 x1ypdohk x78zum5 x1q0g3np x2lah0s xnqzcj9 x1gh759c x1i6fsjq xfvfia3 x1344otq x1de53dj x10wwi4t x1x7e7qh x1n2onr6 x16tdsg8 x1ja2u2z'.split(' ').join('.')}`);
 }
 function mapConfigugationMilesSubmitButtonSelector(){
     // a button that confirms the mile choice
