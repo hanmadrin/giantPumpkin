85e23f8346459d03d054c04acda1cc17039d1c44
hanmadrin <hanmadrin@gmail.com>
2024-06-04 05:03:24 +0600

    save

--- content.js
+++ content.js
@@ -587,15 +587,17 @@ async function scrapingLevelZero(index,number){
 const getItemInformationFromMarketplaceItemPage = ()=>{
     const jsonScripts = document.querySelectorAll('script[type="application/json"]');
     let jsonData = null;
+    let jsonDatas = [];
     for(let i=0;i<jsonScripts.length;i++){
         const fullText = jsonScripts[i].textContent;
         // find "marketplace_product_details_page"
         if(fullText.includes('marketplace_product_details_page')){
-            jsonData = JSON.parse(fullText);
-            break;
+            jsonDatas.push(JSON.parse(fullText));
+            // break;
         }
     }
-    if(jsonData){ 
+    if(jsonDatas.length>0){ 
+        jsonDatas.length>1?jsonData = jsonDatas[1]:jsonData = jsonDatas[0];
         const findNestedKeyValue = (obj, targetKey)=>{
             for (const key in obj) {
               if (obj.hasOwnProperty(key)) {
@@ -612,6 +614,7 @@ const getItemInformationFromMarketplaceItemPage = ()=>{
             }
             return undefined;
         }
+        
         const stateConverter = (shortState)=>{
             // var shortStates = ['WI','IL','TN','MS','AL','FL','GA','SC','NC','KY','VA','IN','MI','OH','PA','NY','ME','NH','VT','MA','RI','CT','NJ','DE','MD','WV'];
             var shortStates = ['WI','IL','TN','FL','GA','SC','NC','KY','VA','IN','MI','OH','PA','NY','ME','NH','VT','MA','RI','CT','NJ','DE','MD','WV','MN',"IA","MO","AR","TX","OK",'KS','ND'];
