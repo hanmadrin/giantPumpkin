a660ae9b940e71c991592f884e931e62ec186d48
Md Hasan Mahmud Rimon <hanmadrin@gmail.com>
2025-04-24 05:17:52 +0600

    save

--- extra.js
+++ extra.js
@@ -158,7 +158,7 @@
     if(action=="productListing"){
         let ex_collected = await getStorageSingleData('ex_collected') || [];
         console.log(`ex_collected: ${ex_collected}`);
-        ex_collected = ex_collected || [];
+        ex_collected = ex_collected!=null? ex_collected:[];
         console.log(`ex_collected: ${ex_collected}`);
         let ex_workId = await getStorageSingleData('ex_workId');
         let ex_apiUrl = await getStorageSingleData('ex_apiUrl');
