1a53e0e627d3da6890542a302dddbd930f29cfc7
Md Hasan Mahmud Rimon <hanmadrin@gmail.com>
2026-01-21 04:36:14 +0600

    valid listings

--- content.js
+++ content.js
@@ -1280,76 +1280,79 @@ function isValidListing(item,index) {
             console.log("dealership")
             return false;
         }
-        // const details = item.querySelectorAll("span:not(:has(*)):not(:empty)");
-        // if (details.length >= 4) {
-        //     console.log("not 4 line details")
-        //     return false;
-        // }
-        // // const [priceText, yearNameText, stateCityText, mileageText] = details.map(elm => elm.innerText);
-        // let priceText, yearNameText, stateCityText, mileageText;
-        // if(details.length==4){
-        //     [priceText, yearNameText, stateCityText, mileageText] = Array.from(details).map(elm => elm.innerText);
-        // }else{
-        //     [priceText, ,yearNameText, stateCityText, mileageText] = Array.from(details).map(elm => elm.innerText);
-        // }
-        // const price = priceText.replace('$', '').replace(',', '')
-        // // yearNameText = 2012 Land Rover range rover evoque
-        // const year = yearNameText.match(/^\d{4}/);
-        // const make = yearNameText.replace(/^\d{4}/, '').trim().split(" ")[0];
-        // const state = stateCityText.split(',')[1].trim();
-        // // 77K miles
-        // const mileage = mileageText.match(/\d+/)[0] * 1000;
+        const details = item.querySelectorAll("span:not(:has(*)):not(:empty)");
+        if (details.length >= 4) {
+            console.log("not 4 line details")
+            return false;
+        }
+        // const [priceText, yearNameText, stateCityText, mileageText] = details.map(elm => elm.innerText);
+        let priceText, yearNameText, stateCityText, mileageText;
+        if(details.length==4){
+            [priceText, yearNameText, stateCityText, mileageText] = Array.from(details).map(elm => elm.innerText);
+        }else if(details.length==5){
+            [priceText, ,yearNameText, stateCityText, mileageText] = Array.from(details).map(elm => elm.innerText);
+        }else{
+            showHTMLOnContentConsole(`invalid details length: ${details.length} at index ${index}`);
+            throw new Error(`invalid details length: ${details.length} at index ${index}`);
+        }
+        const price = priceText.replace('$', '').replace(',', '')
+        // yearNameText = 2012 Land Rover range rover evoque
+        const year = yearNameText.match(/^\d{4}/);
+        const make = yearNameText.replace(/^\d{4}/, '').trim().split(" ")[0];
+        const state = stateCityText.split(',')[1].trim();
+        // 77K miles
+        const mileage = mileageText.match(/\d+/)[0] * 1000;
 
-        // const validStates = ['WI', 'IL', 'TN', 'MS', 'AL', 'FL', 'GA', 'SC', 'NC', 'KY', 'VA', 'IN', 'MI', 'OH', 'PA', 'NY', 'ME', 'NH', 'VT', 'MA', 'RI', 'CT', 'NJ', 'DE', 'MD', 'WV', 'MN', "IA", "MO", "AR", "TX", "OK", 'KS', 'ND'];
-        // const validMakes = [
-        //     "BUICK",
-        //     "CHEVROLET",
-        //     "CHEVY",
-        //     "CHRYSLER",
-        //     "FIAT",
-        //     "FORD",
-        //     "GMC",
-        //     "HONDA",
-        //     "HYUNDAI",
-        //     "JEEP",
-        //     "KIA",
-        //     "LINCOLN",
-        //     "MAZDA",
-        //     "MITSUBISHI",
-        //     "NISSAN",
-        //     "RAM",
-        //     "SCION",
-        //     "SMART",
-        //     "SUBARU",
-        //     "TOYOTA",
-        //     "VOLKSWAGEN"
-        // ];
+        const validStates = ['WI', 'IL', 'TN', 'MS', 'AL', 'FL', 'GA', 'SC', 'NC', 'KY', 'VA', 'IN', 'MI', 'OH', 'PA', 'NY', 'ME', 'NH', 'VT', 'MA', 'RI', 'CT', 'NJ', 'DE', 'MD', 'WV', 'MN', "IA", "MO", "AR", "TX", "OK", 'KS', 'ND'];
+        const validMakes = [
+            "BUICK",
+            "CHEVROLET",
+            "CHEVY",
+            "CHRYSLER",
+            "FIAT",
+            "FORD",
+            "GMC",
+            "HONDA",
+            "HYUNDAI",
+            "JEEP",
+            "KIA",
+            "LINCOLN",
+            "MAZDA",
+            "MITSUBISHI",
+            "NISSAN",
+            "RAM",
+            "SCION",
+            "SMART",
+            "SUBARU",
+            "TOYOTA",
+            "VOLKSWAGEN"
+        ];
 
-        // // 10000-120000 mileage
-        // // 5000 - 50000 price
-        // // 2016 - 2025 year
-        // if (price < 5000 || price > 50000) {
-        //     console.log("invalid price")
-        //     return false;
-        // }
-        // if (year < 2016 || year > 2025) {
-        //     console.log("invalid year")
-        //     return false;
-        // }
-        // if (mileage < 10000 || mileage > 120000) {
-        //     console.log("invalid mileage")
-        //     return false;
-        // }
-        // if (!validMakes.includes(make.toUpperCase())) {
-        //     console.log("invalid make")
-        //     return false;
-        // }
-        // if (!validStates.includes(state)) {
-        //     console.log("invalid state")
-        //     return false;
-        // }
+        // 10000-120000 mileage
+        // 5000 - 50000 price
+        // 2016 - 2025 year
+        if (price < 5000 || price > 50000) {
+            console.log("invalid price")
+            return false;
+        }
+        if (year < 2016 || year > 2025) {
+            console.log("invalid year")
+            return false;
+        }
+        if (mileage < 10000 || mileage > 120000) {
+            console.log("invalid mileage")
+            return false;
+        }
+        if (!validMakes.includes(make.toUpperCase())) {
+            console.log("invalid make")
+            return false;
+        }
+        if (!validStates.includes(state)) {
+            console.log("invalid state")
+            return false;
+        }
 
-        // console.log(item)
+        console.log(item)
         return true;
     }
 function isSpecifiedState(shortForm) {
